f3d7487878895696c1ca644e89703f683a9d12b1
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-06 20:42:23 -0700

    rename Job to JobList

--- src/common/RoutesList.js
+++ src/common/RoutesList.js
@@ -2,7 +2,7 @@ import {Route, Routes, Navigate} from 'react-router-dom';
 import Homepage from '../common/Homepage';
 import CompanyList from '../company/CompanyList';
 import CompanyDetail from '../company/CompanyDetail';
-import Jobs from '../job/Jobs';
+import JobList from '../job/JobList';
 
 function RoutesList() {
   return (
@@ -10,7 +10,7 @@ function RoutesList() {
       <Route element= { <Homepage />} path="/" />
       <Route element= { <CompanyList />} path="/companies" />
       <Route element= { <CompanyDetail />} path="/companies/:name" />
-      <Route element= { <Jobs />} path="/jobs" />
+      <Route element= { <JobList />} path="/jobs" />
       <Route element= { <Navigate to="/" />} path="*" />
     </Routes>
   );
---
+++ src/job/JobList.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import JoblyApi from "../api";
+import JobCard from "./JobCard";
+import Search from "../common/Search";
+
+function JobList(){
+
+
+  return (
+    <h1>Jobs</h1>
+  )
+}
+
+export default JobList;
