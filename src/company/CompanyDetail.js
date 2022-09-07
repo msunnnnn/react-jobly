826b6f46583320fd33731d64787e35017fcfd637
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-06 20:36:26 -0700

    complete CompanyDetail and JobCard

--- src/company/CompanyDetail.js
+++ src/company/CompanyDetail.js
@@ -1,12 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+import JoblyApi from "../api";
+import JobCard from "../job/JobCard";
+
+/**
+ * Displays company's details and company's job listings
+ *
+ * (Option: CompanyList ->) CompanyDetail-> JobCard
+ */
 
 function CompanyDetail() {
 
-  const [company, setCompany] = useState();
+  //gets company handle from params
+  const { name } = useParams();
+
+  const [companyDetails, setCompanyDetails] = useState({
+    isLoading: true,
+    companyName: "",
+    companyDescription: "",
+    jobs: []
+  });
+
+
+  /** make API call to get company details */
+  useEffect(function getCompanyDetailsWhenMounted() {
+    async function getCompanyDetails() {
+      const response = await JoblyApi.getCompany(name);
+      setCompanyDetails({
+        isLoading: false,
+        companyName: response.name,
+        companyDescription: response.description,
+        jobs: response.jobs
+      });
+      console.log(response)
+    }
+    getCompanyDetails();
+  }, [name]);
+  //Line 30:
+  //it gives an error if array is empty, googled it and saw to add the params in there
+  //https://ui.dev/react-router-url-parameters
 
 
 
+  if (companyDetails.isLoading) {
+    <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+    <h1>{companyDetails.companyName}</h1>
+    <p>{companyDetails.companyDescription}</p>
+
+      {companyDetails.jobs.map(c => (
+        <JobCard key={c.id} job={c} />))
+      }
+    </>
+  );
 
 }
 
---
+++ src/job/JobCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+/** Displays job listings
+ *
+ * Prop
+ * -job {title, companyName, salary, equity}, companyName could be empty
+ *
+ *CompanyDetail -> JobCard
+*/
+function JobCard({job}){
+  const {title, companyName, salary, equity} = job
+
+  return (
+    <>
+    <h4>{title}</h4>
+    <h5>{companyName}</h5>
+    <p>Salary: {salary}</p>
+    <p>Equity: {equity}</p>
+    </>
+  )
+}
+
+export default JobCard
