37ffad526021b4d71b8e1246bb03ac433ade5194
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-06 20:50:53 -0700

    complete JobList (copy and pasted from CompanyList)

--- src/job/JobList.js
+++ src/job/JobList.js
@@ -5,10 +5,53 @@ import Search from "../common/Search";
 
 function JobList(){
 
+  const [jobs, setJobs] = useState({
+    isLoading: true,
+    data : []
+  })
+
+  useEffect(function getJobsWhenMounted() {
+    async function getJobs() {
+      const response = await JoblyApi.request("jobs");
+      setJobs({
+        isLoading: false,
+        data: response.jobs
+      })
+    }
+    getJobs();
+  }, []);
+
+
+  async function searchJobs(searchTerm) {
+    let data = searchTerm !== ''
+             ? {name: searchTerm}
+             : {};
+    const response = await JoblyApi.request("jobs", data);
+
+    setJobs({
+      isLoading: false,
+      data: response.jobs
+    });
+  }
+
+
+
+  if (jobs.isLoading) {
+    return (
+      <h1>Loading...</h1>
+    );
+  }
 
   return (
-    <h1>Jobs</h1>
-  )
+    <>
+      <Search searchBy={searchJobs}/>
+
+      {jobs.data.map(j => (
+        <JobCard key={j.id} job={j} />))
+      }
+    </>
+  );
+
 }
 
 export default JobList;
